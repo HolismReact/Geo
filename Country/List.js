1bfa4c51fb6e1f74e0e16931d71436f49ce0c77d
Saeed Nemati <sn.1361@gmail.com>
2021-06-23 17:53:30 +0430

    using react-admin

--- Country/List.js
+++ Country/List.js
@@ -1,9 +1,15 @@
-const CountryList = () => {
-    return (
-        <div>
-            Country list
-        </div>
-    );
-}
+import { List, Datagrid, TextField } from 'react-admin';
 
-export default CountryList;
+export const CountryList = props => (
+    <List {...props}>
+        <Datagrid rowClick="show">
+            <TextField source="id" />
+            <TextField source="name" />
+            <TextField source="officialName" />
+            <TextField source="alternativeName" />
+            <TextField source="isoTwoLetterCode" />
+            <TextField source="isoThreeLetterCode" />
+            <TextField source="cctld" />
+        </Datagrid>
+    </List>
+);
