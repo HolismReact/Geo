a61da0910831e1d9bfde7ec2ce52fa548b4655cf
Sanam <35029825+pourmand2010@users.noreply.github.com>
2021-10-08 18:59:41 +0330

    committed by script

--- Resources.js
+++
@@ -1,8 +0,0 @@
-import { CountryList } from "./Geo/Country/List";
-import { Resource } from 'react-admin';
-
-var resources = [
-    <Resource name='countries' list={CountryList} />
-]
-
-export default resources;
